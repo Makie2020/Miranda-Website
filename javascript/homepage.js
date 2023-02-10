b4ba6a88eaad6e2570da24ef9ac5a47566b71200
Marieke <69918471+Makie2020@users.noreply.github.com>
2023-02-10 09:44:37 +0100

    upload data

---
+++ javascript/homepage.js
@@ -0,0 +1,6 @@
+function buttonClick() {
+  console.log('hello')
+  let navbar = document.getElementById('navbar');
+  let responsive_class_name = 'responsive'
+  navbar.classList.toggle(responsive_class_name)
+}
