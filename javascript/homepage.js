77b355e991037dd1d8d7cb81338291a1cff6909c
Marieke <69918471+Makie2020@users.noreply.github.com>
2023-02-10 14:06:12 +0100

    upload swipers

--- javascript/homepage.js
+++ javascript/homepage.js
@@ -1,6 +1,4 @@
 function buttonClick() {
-  console.log('hello')
   let navbar = document.getElementById('navbar');
   let responsive_class_name = 'responsive'
-  navbar.classList.toggle(responsive_class_name)
-}
+  navbar.classList.toggle(responsive_class_name)}
--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,7 +1,17 @@
-import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js' 
-
-var swiper = new Swiper('.swiper-container', {    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-});
+const swiper_text = new Swiper(".swiper_text", {
+  loop: true, 
+})
+const swiper_main = new Swiper('.swiper_main', {
+  loop: true,                         
+  pagination: {
+      el: '.swiper-pagination',
+  },                  
+  navigation: {                       
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {                           
+      swiper: swiper_text,  
+    
+  },                                  
+})
